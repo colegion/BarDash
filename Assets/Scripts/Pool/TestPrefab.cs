301f58f08486b8efeb0253c6f2d958d1e71cdf7a
Ekrep <76533884+Ekrep@users.noreply.github.com>
2024-11-15 13:29:52 +0300

    Test

--- Assets/Scripts/Pool/TestPrefab.cs
+++ Assets/Scripts/Pool/TestPrefab.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestPrefab : MonoBehaviour
+public class TestPrefab : MonoBehaviour, IPoolable
 {
+    public void OnAssignPool()
+    {
+        Debug.Log("Assigned");
+    }
+
+    public void OnCretaedForPool()
+    {
+        Debug.Log("CreatedForPool");
+    }
+
+    public void OnReleasePool()
+    {
+        Debug.Log("ReleasedFromPool");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
